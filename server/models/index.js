d07e34efaaee66b71f8e0819b7060186858e8529
Miacarmen <maitreyacarmen@gmail.com>
2022-08-15 15:53:41 -0700

    User schema created, base model and schema files created

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Event = require('./Event');
+const Checklist = require('./Checklist');
+const ChecklistItem = require('./Checklist-Item');
+
+module.exports = { User, Event, Checklist, ChecklistItem };
